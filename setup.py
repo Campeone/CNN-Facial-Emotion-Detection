97a4599b2a13fe24afe853597c97b024bef1d81c
Campeone <Ojotimilehin01@gmail.com>
2023-12-05 13:10:08 +0100

    Project Structure created with Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Facial Emotion Detection is a deep learning project aimed at developing a Convolutional Neural Network (CNN) for facial emotion recognition. The Computer Vision project was trained to accurately detect and classify facial expressions such as "Anger", "Disgust", "Fear", "Happiness", "Neutrality", "Sadness", and "Surprise", leveraging datasets with labeled facial images.The repository contains the Train and Test datasets,implementation code, the trained model, and the Flask deployment code.',
+    author='Campeone',
+    license='MIT',
+)
